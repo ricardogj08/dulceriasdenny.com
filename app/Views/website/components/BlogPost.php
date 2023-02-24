1adb0620ededc451fe01c51b5e6fed591b3d8b77
francisco-solis99 <franciscosoliscss@gmail.com>
2023-02-24 16:21:46 -0600

    [Create] blog post component

---
+++ app/Views/website/components/BlogPost.php
@@ -0,0 +1,13 @@
+<article class="lg:flex lg:justify-between">
+    <picture class="lg:w-1/2 max-w-lg min-w-[280px] w-3/4 mx-auto block">
+        <a href="">
+            <img src="/images/blog/post.webp" alt="Post image" class="w-full object-cover">
+        </a>
+    </picture>
+    <div class="lg:w-7/12 mt-4 lg:mt-0 lg:ml-7 text-center sm:text-left">
+        <h3 class="text-denny-dark-500 font-black text-2xl">
+            <a href="">Titulo del articulo</a>
+        </h3>
+        <p class="mt-2 text-justify sm:text-left">Convallis mi eros leo laoreet et eu consectetur tempor venenatis nulla a sit parturient eget. Nam convallis venenatis vel aliquet ullamcorper sagittis dictum dis aliquet a parturient parturient varius porttitor elit est faucibus in mi. Proin cum consectetur a ut nibh sociis purus at mattis justo vestibulum sociosqu a erat senectus scelerisque metus nunc magna inceptos dictumst eu fames sagittis magna. Felis suspendisse dignissim sed ullamcorper gravida a elementum ad nullam ut placerat dis laoreet eget et nulla scelerisque a penatibus congue.</p>
+    </div>
+</article>
