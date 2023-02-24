b3ab0aa88ae4cda2410cb7db308907e35241157c
francisco-solis99 <franciscosoliscss@gmail.com>
2023-02-24 13:56:59 -0600

    [Add] the footer component

---
+++ app/Views/website/components/Footer.php
@@ -0,0 +1,96 @@
+<footer class="bg-denny-light-200 text-denny-dark-400 mt-10">
+    <section class="grid place-items-center content-center py-4 min-h-[350px]">
+        <div class="container flex flex-col md:flex-row md:justify-between gap-8">
+            <div class="md:w-3/4">
+                <h2 class="font-bold text-denny-blue-300 text-2xl mb-4 text-center md:text-left">Una Sucursal Cerca de ti </h2>
+                <div class="flex gap-4 justify-between flex-wrap">
+                    <ul class="text-sm flex flex-col gap-1">
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Silao</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Dolores Hidalgo</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Abastos 3</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Pedro Moreno</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Insurgentes</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Oaxaca</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Jacarandas</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Esperanza</li>
+                    </ul>
+                    <ul class="text-sm flex flex-col gap-1">
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Silao</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Dolores Hidalgo</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Abastos 3</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Pedro Moreno</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Insurgentes</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Oaxaca</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Jacarandas</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Esperanza</li>
+                    </ul>
+                    <ul class="text-sm flex flex-col gap-1">
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Silao</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Dolores Hidalgo</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Abastos 3</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Pedro Moreno</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Insurgentes</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Oaxaca</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Jacarandas</li>
+                        <li><strong class="text-denny-blue-300">Gto.</strong>- Esperanza</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="md:w-[20%]">
+                <h3 class="font-bold text-denny-blue-300 text-lg mb-4 text-center md:text-left">Soporte y Politicas</h3>
+                <ul class="text-sm">
+                    <li><a href="" class="transition-opacity hover:opacity-80">Preguntas Frecuentes</a></li>
+                    <li class="mt-1"><a href="" class="transition-opacity hover:opacity-80">Terminos y Condiciones</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div class="min-h-[100px] flex items-center bg-denny-blue-300 text-white">
+        <div class="container flex justify-between items-center">
+            <div class="flex w-3/4 gap-4">
+                <div class="social flex items-center">
+                    <a href="https://www.instagram.com/dulceriasdenny/" target="_blank">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="27" fill="none">
+                                <path fill="#fff" fill-rule="evenodd" d="M26.9111 7.9331c-.1024-2.1629-.5964-4.0787-2.1809-5.6572C23.1517.6974 21.2358.2033 19.0729.0949c-2.2292-.1265-8.9106-.1265-11.1398 0-2.1568.1024-4.0727.5964-5.6572 2.175C.6913 3.8482.2033 5.7641.0949 7.927c-.1265 2.2292-.1265 8.9106 0 11.1398.1024 2.1629.5964 4.0788 2.181 5.6572 1.5845 1.5785 3.4943 2.0726 5.6572 2.181 2.2292.1265 8.9106.1265 11.1398 0 2.1629-.1024 4.0788-.5964 5.6573-2.181 1.5785-1.5784 2.0725-3.4943 2.1809-5.6572.1266-2.2292.1266-8.9046 0-11.1338Zm-6.1995-.024a1.611 1.611 0 0 0 1.6147-1.6147c0-.8917-.723-1.6147-1.6147-1.6147-.8916 0-1.6146.723-1.6146 1.6147a1.611 1.611 0 0 0 1.6146 1.6146ZM13.506 6.5774c-3.8317 0-6.9224 3.0908-6.9224 6.9225 0 3.8318 3.0907 6.9225 6.9224 6.9225 3.8318 0 6.9225-3.0907 6.9225-6.9225 0-3.8317-3.0907-6.9225-6.9225-6.9225Zm0 11.423c-2.4762 0-4.5005-2.0183-4.5005-4.5005 0-2.4822 2.0183-4.5005 4.5005-4.5005 2.4822 0 4.5005 2.0183 4.5005 4.5005 0 2.4822-2.0243 4.5005-4.5005 4.5005Zm7.9587 6.0248c1.1869-.476 2.0967-1.3857 2.5666-2.5666.6046-1.5193.571-4.8425.5491-6.9967-.0036-.3574-.0069-.6826-.0069-.962 0-.2795.0033-.6046.0069-.9618.0219-2.1531.0555-5.4725-.5491-6.997-.476-1.1868-1.3857-2.0965-2.5666-2.5665-1.524-.6013-4.8634-.5693-7.017-.5486-.3494.0034-.6675.0064-.9417.0064-.2795 0-.6046-.0033-.9618-.007-2.1531-.0217-5.4725-.0554-6.9969.5492-1.1869.476-2.0966 1.3857-2.5665 2.5666-.6014 1.524-.5693 4.8634-.5487 7.017.0034.3494.0064.6675.0064.9417 0 .2795-.0033.6046-.0069.9618-.0218 2.1531-.0554 5.4725.5492 6.9969.476 1.1869 1.3857 2.0966 2.5665 2.5666 1.524.6013 4.8634.5693 7.017.5486.3494-.0033.6676-.0064.9417-.0064.2795 0 .6046.0033.9619.0069 2.1531.0218 5.4725.0555 6.9968-.5491Z" clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                    </a>
+                    <a href="https://es-la.facebook.com/dennydulcerias/" target="_blank" class="ml-7">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="27" fill="none">
+                                <path fill="#fff" d="M27.1643 13.5822C27.1643 6.0792 21.0852 0 13.5822 0S0 6.0791 0 13.5822C0 20.3612 4.9668 25.9802 11.4599 27v-9.4916H8.0096v-3.9262H11.46v-2.9925c0-3.4038 2.0264-5.2839 5.13-5.2839 1.4864 0 3.0407.265 3.0407.265v3.3408h-1.7131c-1.6868 0-2.2131 1.0472-2.2131 2.1212v2.5494h3.7668l-.6024 3.9262h-3.1644V27c6.4931-1.0198 11.4599-6.6388 11.4599-13.4178Z" />
+                            </svg>
+
+                        </span>
+                    </a>
+                </div>
+                <p class="mx-auto flex flex-col text-xs lg:text-sm">
+                    <small>© Dulcerías Denny 2022 • Todos los derechos reservados.
+                        <strong>
+                            <a href="/privacidad" target="_blank">Aviso de Privacidad</a>
+                        </strong>
+                    </small>
+                    <small>Diseño y desarrollo web por: <a href="https://www.genotipo.com/" target="_blank">Genotipo®</a></small>
+                </p>
+                <p class="text-xs mx-auto"></p>
+            </div>
+            <div class="chat">
+                <button>
+                    <span class="bg-white inline-block p-1.5 rounded-full shadow-sm">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 39 39" fill="none">
+                            <path fill="url(#a)" d="M7.1828 32.0542V39l6.5445-3.6923c1.8243.5218 3.7635.8034 5.7727.8034 10.7703 0 19.5-8.0844 19.5-18.0555C39 8.0837 30.2703 0 19.5 0 8.7304 0 0 8.0837 0 18.0556c0 5.6464 2.7993 10.6882 7.1828 13.9986Z" />
+                            <path fill="#fff" d="M17.5044 13 7 24l9.5599-5.1876L21.5549 24 32 13l-9.4538 5.0968L17.5044 13Z" />
+                            <defs>
+                                <linearGradient id="a" x1="2.1762" x2="2.1762" y1="2.6364" y2="37.284" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#00C6FF" />
+                                    <stop offset="1" stop-color="#0068FF" />
+                                </linearGradient>
+                            </defs>
+                        </svg>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+</footer>
