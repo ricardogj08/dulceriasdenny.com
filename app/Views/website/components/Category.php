f95f2157bc2540a90af1ee7b7d27aeddd0249e99
francisco-solis99 <franciscosoliscss@gmail.com>
2023-02-23 18:10:48 -0600

    [Add] images to use in the website

---
+++ app/Views/website/components/Category.php
@@ -0,0 +1,9 @@
+
+<article class="w-full">
+  <a href="">
+    <figure class="flex items-center justify-center max-w-[120px] mx-auto">
+      <img src="<?= esc($category['image']) ?>" alt="Category <?= esc($category['name']) ?>" class="w-full h-full object-cover mr-4">
+      <figcaption class="md:text-lg text-denny-dark-400"><?= esc($category['name']) ?></figcaption>
+    </figure>
+  </a>
+</article>
