85d01a665756953f179b44667c07b8c7c45b552e
francisco-solis99 <franciscosoliscss@gmail.com>
2023-02-24 18:28:26 -0600

    [Edit] the route for the error 404 page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,7 +14,7 @@ $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
-$routes->set404Override(ENVIRONMENT === 'production' ? 'App\Controllers\Website\Pages::error404' : null);
+$routes->set404Override(ENVIRONMENT === 'production' ? 'App\Controllers\Website\Pages::error404' : 'App\Controllers\Website\Pages::error404');
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
