5976598b91b948b927e2b90bee5f84655e34beb1
francisco-solis99 <franciscosoliscss@gmail.com>
2023-02-27 18:06:16 -0600

    [Create] the pagination component

---
+++ app/Views/website/components/Pagination.php
@@ -0,0 +1,21 @@
+<div class="pagination flex items-center justify-end gap-x-2">
+  <button id="btn-pagination-prev">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#0D6FFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <polyline points="15 6 9 12 15 18" />
+    </svg>
+  </button>
+  <ul class="pagination-list flex gap-x-4">
+    <li class="pagination-item"><a href="#" class="text-denny-dark-300 font-bold hover:text-denny-blue-300">1</a></li>
+    <li class="pagination-item"><a href="#" class="text-denny-dark-300 hover:text-denny-blue-300">2</a></li>
+    <li class="pagination-item"><a href="#" class="text-denny-dark-300 hover:text-denny-blue-300">3</a></li>
+    <li class="pagination-item"><a href="#" class="text-denny-dark-300 hover:text-denny-blue-300">4</a></li>
+    <li class="pagination-item"><a href="#" class="text-denny-dark-300 hover:text-denny-blue-300">5</a></li>
+  </ul>
+  <button id="btn-pagination-next">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#0D6FFF" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <polyline points="9 6 15 12 9 18" />
+    </svg>
+  </button>
+</div>
