c5043c27faed22b5d3690d26afa090bd722fe643
Ricardo García Jiménez <ricardo@genotipo.com>
2023-03-02 15:11:34 -0600

    feat: Agrega migración y modelo de la tabla de artículos

---
+++ app/Database/Migrations/2023-03-02-201916_AddPosts.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+/**
+ * Migración que crea la tabla de artículos.
+ */
+class AddPosts extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'bigint',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'slug' => [
+                'type'       => 'varchar',
+                'constraint' => 256,
+                'unique'     => true,
+            ],
+            'cover' => [
+                'type'       => 'varchar',
+                'constraint' => 64,
+                'unique'     => true,
+            ],
+            'title' => [
+                'type'       => 'varchar',
+                'constraint' => 256,
+            ],
+            'excerpt' => [
+                'type'       => 'varchar',
+                'constraint' => 512,
+            ],
+            'body' => [
+                'type' => 'mediumtext',
+            ],
+            'active' => [
+                'type'       => 'tinyint',
+                'constraint' => 1,
+                'unsigned'   => true,
+                'default'    => true,
+            ],
+            'started_at' => [
+                'type' => 'date',
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'datetime',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'datetime',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addPrimaryKey('id');
+
+        $this->forge->createTable('posts', true);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('posts', true);
+    }
+}
---
+++ app/Models/PostModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+/**
+ * Modelo que representa la tabla de artículos.
+ */
+class PostModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'posts';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $allowedFields    = [
+        'slug',
+        'cover',
+        'title',
+        'excerpt',
+        'body',
+        'active',
+        'started_at',
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+    // Validation
+    protected $skipValidation       = true;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = false;
+}
