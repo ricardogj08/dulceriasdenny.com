97eece3f88fbfd95e5d407143ad21a842dde7dc4
Ricardo García Jiménez <ricardo@genotipo.com>
2023-03-02 11:00:32 -0600

    feat: Agrega maquetación desktop y móvil del email de recuperación de contraseñas

--- app/Views/backend/emails/recovery.php
+++ app/Views/backend/emails/recovery.php
@@ -2,13 +2,34 @@
 
 <?= $this->section('head') ?>
     <title>
+        Recupera tu contraseña
     </title>
 
     <meta
         name="description"
-        content=""
+        content="Recupera tu contraseña."
     >
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
+    <h1 class="text-2xl font-bold mb-4">
+        Recupera tu contraseña
+    </h1>
+
+    <p class="text-justify mb-4">
+        ¡Hola <?= esc(word_limiter('Ricardo García Jiménez', 1, '')) ?>!,
+        presiona el siguiente botón para completar tu solicitud de recuperación de contraseña:
+    </p>
+
+    <p class="text-center mb-2">
+        <a href="<?= url_to('backend.auth.recovery', 1, 'test') ?>" class="btn btn-primary btn-wide">
+            Recuperar contraseña
+        </a>
+    </p>
+
+    <p class="text-center">
+        <small>
+            Por favor ignora este mensaje si no has realizado esta solicitud.
+        </small>
+    </p>
 <?= $this->endSection() ?>
